ac84b37ef3f7e7debac17749537c4d3a6c1e3032
Yellow Moon <yellowmoonastronaut@gmail.com>
2022-09-20 14:03:22 -0300

    update open time

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,25 @@ function handleMenu () {
     document.querySelector("#openMenu").classList.toggle("hiden")
 }
 
+const newDate = new Date();
+
 // DOM Loaded
-// document.addEventListener('DOMContentLoaded', () => {
-//    
-// })
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Handle open time
+    if (
+        ( 
+            ( newDate.getDay() >= 1 && newDate.getDay() <= 5 )
+            && ( newDate.getHours() >= 17 && newDate.getHours() <= 23 )
+        )
+        ||
+        (
+            ( newDate.getDay() === 0 || newDate.getDay() === 6 )
+            && ( ( newDate.getHours() >= 18 && newDate.getMinutes() >= 30 ) && newDate.getHours() <= 23 )
+        )
+
+    ) {
+        document.querySelector('#Card').classList.add('open')
+    }
+
+})
