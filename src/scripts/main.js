05c79701b607f1c3c99404e4da479593c39bc1d2
Yellow Moon <yellowmoonastronaut@gmail.com>
2022-09-20 20:05:15 -0300

    update Carte

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,33 @@ function handleMenu () {
 
 const newDate = new Date();
 
+async function getCarte () {
+    const data = await fetch('https://api.brchallenges.com/api/empire-burger/menu')
+        .then((response) => { return response.json()})
+
+    let carteItens = ""
+
+    data.map( data => {
+        carteItens +=
+        `
+        <div>
+            <div class="burger-title">
+                <span>${data.plate}</span>
+                <div></div>
+                <span>R$${parseFloat(data.price).toFixed(2)}</span>
+            </div>
+            <p>${data.ingredients}</p>
+        </div>
+        `
+    })
+
+    document.querySelector("#carte-itens").innerHTML = carteItens
+}
+
 // DOM Loaded
 document.addEventListener('DOMContentLoaded', () => {
+
+    getCarte()
 
     // Handle open time
     if (
